4635ee8f552ae4b8ee44d51e0ab65078223af1d7
Artur Trifonov <artur@uniteddesign.ru>
2019-07-18 13:27:25 +0300

    layout and graphic created

--- dist/index.php
+++ dist/index.php
@@ -8,7 +8,216 @@
     <?include('includes/_header.php');?>
 
     <main class="main">
-        
+        <section class="teaser">
+            <div class="teaser__title">
+                <h1>Обучающие видеуроки и статьи о программе</h1>
+            </div>
+
+            <div class="teaser__text">
+                <p>В этой главе вас ждут познавательные статьи, в которых рассмотрены различные функции программы «Ускоритель компьютера».</p>
+            </div>
+
+            <div class="teaser__video video">
+                <div class="video__img">
+                    <img src="img/video.jpg" alt="Ускоритель компьютера" />
+                </div>
+                <a href="#" class="video__link">Ускоритель компьютера</a>
+                <p class="video__text">Видеообзор программы</p>
+            </div>
+        </section>
+
+        <section class="content">
+            <h2>Изучаем программу</h2>
+            <p>Мы подготовили для вас подборку полезных видеоуроков о работе в "Ускоритель компютера". В них наглядно показаны основные функции программы, а также даны практические рекомендации по обработке и печати фотографий. Просматривая ролики, вы сможете освоить редактор как минимум в два раза быстрее!</p>
+        </section>
+
+        <section class="promo-articles">
+            <h3>Выбор пользователей</h3>
+
+            <ul class="promo-articles__list">
+                <li class="promo-articles__item">
+                    <div class="promo-articles__img">
+                        <img src="img/articles/defragmentation.jpg" alt="Дефрагментация жесткого диска: все типовые вопросы" />
+                    </div>
+                    <a href="#" class="promo-articles__link">Дефрагментация жесткого диска: все типовые вопросы</a>
+                </li>
+
+                <li class="promo-articles__item">
+                    <div class="promo-articles__img">
+                        <img src="img/articles/speed.jpg" alt="Ускорение компьютера ОСWindows: топ 10 программ" />
+                    </div>
+                    <a href="#" class="promo-articles__link">Ускорение компьютера ОСWindows: топ 10 программ</a>
+                </li>
+
+                <li class="promo-articles__item">
+                    <div class="promo-articles__img">
+                        <img src="img/articles/ram.jpg" alt="Что такое оперативная память и для чего она нужна" />
+                    </div>
+                    <a href="#" class="promo-articles__link">Что такое оперативная память и для чего она нужна</a>
+                </li>
+
+                <li class="promo-articles__item">
+                    <div class="promo-articles__img">
+                        <img src="img/articles/delete.jpg" alt="Как удалить папку или файл, если они не удаляются" />
+                    </div>
+                    <a href="#" class="promo-articles__link">Как удалить папку или файл, если они не удаляются</a>
+                </li>
+
+                <li class="promo-articles__item">
+                    <div class="promo-articles__img">
+                        <img src="img/articles/cookie.jpg" alt="Что такое файлы cookie, все что нужно знать" />
+                    </div>
+                    <a href="#" class="promo-articles__link">Что такое файлы cookie, все что нужно знать</a>
+                </li>
+
+                <li class="promo-articles__item">
+                    <div class="promo-articles__img">
+                        <img src="img/articles/observer.jpg" alt="Как отключить слежку в интернет браузере Mozilla" />
+                    </div>
+                    <a href="#" class="promo-articles__link">Как отключить слежку в интернет браузере Mozilla</a>
+                </li>
+
+                <li class="promo-articles__item">
+                    <div class="promo-articles__img">
+                        <img src="img/articles/windows.jpg" alt="Оптимизация ОС Windows 10: полезные лайфхаки" />
+                    </div>
+                    <a href="#" class="promo-articles__link">Оптимизация ОС Windows 10: полезные лайфхаки</a>
+                </li>
+
+                <li class="promo-articles__item">
+                    <div class="promo-articles__img">
+                        <img src="img/articles/rating.jpg" alt="Как удалить папку или файл, если они не удаляются" />
+                    </div>
+                    <a href="#" class="promo-articles__link">Как удалить папку или файл, если они не удаляются</a>
+                </li>
+            </ul>
+        </section>
+
+        <section class="articles">
+            <div class="srticles__section">
+                <div class="articles__ico">
+                    <img src="img/ico/speed.svg" alt="Работа с ускорителем компьютера" />
+                </div>
+                <h4 class="articles__title">Работа с ускорителем компьютера</h4>
+                <ul class="articles__list">
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Однажды одна маленькая строчка рыбного текста</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">По имени Lorem ipsum решила выйти</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Великий Оксмокс предупреждал ее о злых запятых</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Диких знаках вопроса и коварных точках с запятой</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Далеко-далеко за словесными горами в стране</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">В стране гласных и согласных живут рыбные тексты</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Вдали от всех живут они в буквенных домах</a>
+                    </li>
+                </ul>
+                <button type="button" class="articles__button">Больше статей</button>
+            </div>
+
+            <div class="srticles__section">
+                <div class="articles__ico">
+                    <img src="img/ico/secret.svg" alt="Секреты оптимизации ОС Windows" />
+                </div>
+                <h4 class="articles__title">Секреты оптимизации ОС Windows</h4>
+                <ul class="articles__list">
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Далеко-далеко за словесными горами в стране</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">В стране гласных и согласных живут рыбные тексты</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Вдали от всех живут они в буквенных домах</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">На берегу Семантика большого языкового океана</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">В стране гласных и согласных живут рыбные тексты</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Вдали от всех живут они в буквенных домах</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">На берегу Семантика большого языкового океана</a>
+                    </li>
+                </ul>
+                <button type="button" class="articles__button">Больше статей</button>
+            </div>
+
+            <div class="srticles__section">
+                <div class="articles__ico">
+                    <img src="img/ico/secure.svg" alt="Как ускорить работу ПК: лайфхаки" />
+                </div>
+                <h4 class="articles__title">Как ускорить работу ПК: лайфхаки</h4>
+                <ul class="articles__list">
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Далеко-далеко за словесными горами в стране</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">В стране гласных и согласных живут рыбные тексты</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Вдали от всех живут они в буквенных домах</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">На берегу Семантика большого языкового океана</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Далеко-далеко за словесными горами в стране</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">В стране гласных и согласных живут рыбные тексты</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Вдали от всех живут они в буквенных домах</a>
+                    </li>
+                </ul>
+                <button type="button" class="articles__button">Больше статей</button>
+            </div>
+
+            <div class="srticles__section">
+                <div class="articles__ico">
+                    <img src="img/ico/bugs.svg" alt="Устранение ошибок ОС Windows" />
+                </div>
+                <h4 class="articles__title">Устранение ошибок ОС Windows</h4>
+                <ul class="articles__list">
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Однажды одна маленькая строчка рыбного текста</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">По имени Lorem ipsum решила выйти</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Великий Оксмокс предупреждал ее о злых запятых</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Диких знаках вопроса и коварных точках с запятой</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">В стране гласных и согласных живут рыбные тексты</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">Вдали от всех живут они в буквенных домах</a>
+                    </li>
+                    <li class="articles__item">
+                        <a href="#" class="articles__link">На берегу Семантика большого языкового океана</a>
+                    </li>
+                </ul>
+                <button type="button" class="articles__button">Больше статей</button>
+            </div>
+        </section>
     </main>
 
     <?include('includes/_footer.php');?>
