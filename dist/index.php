e6a7fd6b0435efdf1f8def8461466508bdf00c87
Artur Trifonov <artur@uniteddesign.ru>
2019-07-18 12:32:07 +0300

    starter project is ready

--- dist/index.php
+++ dist/index.php
@@ -2,15 +2,14 @@
 <html lang="ru">
 <head>
     <?include('includes/_head-includes.php');?>
-
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <?include('includes/_header.php');?>
 
-    <div class="teaser">
-        Hi
-
-    </div>
+    <main class="main">
+        
+    </main>
 
     <?include('includes/_footer.php');?>
     <!-- Scripts -->
